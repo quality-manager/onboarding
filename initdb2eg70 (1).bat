6866b29a2276351e24aa1b58c8ed6026258f9870
anshulkulkarni17 <30462439+anshulkulkarni17@users.noreply.github.com>
2020-02-20 14:23:37 +0530

    Add files via upload

---
+++ initdb2eg70 (1).bat
@@ -0,0 +1,11 @@
+db2cmd -i -w db2clpsetcp 
+db2 create database eg70jts using codeset UTF-8 territory en PAGESIZE 16384
+db2 create database eg70ccm using codeset UTF-8 territory en PAGESIZE 16384
+db2 create database eg70qm using codeset UTF-8 territory en PAGESIZE 16384
+db2 create database eg70rm using codeset UTF-8 territory en PAGESIZE 16384
+db2 create database eg70lqe using codeset UTF-8 territory en PAGESIZE 32768
+db2 create database eg70ldx using codeset UTF-8 territory en PAGESIZE 32768
+db2 create database eg70gc using codeset UTF-8 territory en PAGESIZE 16384
+db2 create database eg70dw using codeset UTF-8 territory en PAGESIZE 16384
+db2 create database eg70dcc using codeset UTF-8 territory en PAGESIZE 16384
+
